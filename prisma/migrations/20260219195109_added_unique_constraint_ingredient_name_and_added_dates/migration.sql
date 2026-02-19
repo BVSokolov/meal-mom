495ee8dcd90fe9df972cb28e1c607235eda5278f
Bran <sokolov.branimir@gmail.com>
2026-02-19 20:51:54 +0100

    updated ingredient schema + added unique constraints on the name + added created and modified dates

---
+++ prisma/migrations/20260219195109_added_unique_constraint_ingredient_name_and_added_dates/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Ingredient` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "Ingredient" ADD COLUMN     "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "modifiedAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Ingredient_name_key" ON "Ingredient"("name");
